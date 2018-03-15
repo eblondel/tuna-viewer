a1cbba3be474b27254d3a8e13f25fcbc166173e5
eblondel <emmanuel.blondel1@gmail.com>
2018-03-15 20:11:03 +0100

    update tuna-viewer

--- src/js/main.js
+++ src/js/main.js
@@ -19,12 +19,11 @@ $(document).ready(function(){
 				wmsUrl: "http://geo.vliz.be/geoserver/MarineRegions/wms", layer: "MarinRegions:eez_boundaries",
 				visible: false, showLegend: true, opacity: 0.6, tiled: true, cql_filter: undefined
 			},
-			//FAO FI Geoserver doesn't support CORS	
-			/*{
+			{
 				group: 0, id: "fsa", title: "FAO major areas & breakdown",
 				wmsUrl: "http://www.fao.org/figis/geoserver/area/wms", layer: "area:FAO_AREAS",
 				visible: false, showLegend: true, opacity: 0.9, tiled: true, cql_filter: undefined
-			},*/
+			},
 			{
 				group: 0, id: "grid1x1", title: "Grid 1x1 (CWP)",
 				wmsUrl: "https://tunaatlas.d4science.org/geoserver/tunaatlas/wms", layer: "tunaatlas:grid1x1,tunaatlas:continent",
@@ -38,7 +37,7 @@ $(document).ready(function(){
 			{
 				group: 0, id: "marineareas", title: "Marine areas",
 				wmsUrl: "https://tunaatlas.d4science.org/geoserver/tunaatlas/wms", layer: "tunaatlas:MarineAreas",
-				visible: true, showLegend: true, opacity: 0.9, tiled: true, cql_filter: undefined
+				visible: true, showLegend: false, opacity: 0.9, tiled: true, cql_filter: undefined
 			}
 		]
 	},{
